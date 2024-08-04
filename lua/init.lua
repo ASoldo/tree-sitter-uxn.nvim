7b8912bd6863bfe5906775406283b2977d2c8696
Rootster <soldo.andrej@gmail.com>
2024-08-05 00:51:24 +0200

    Init project

---
+++ lua/init.lua
@@ -0,0 +1,35 @@
+local function setup()
+	vim.filetype.add({
+		extension = {
+			uxn = "uxn",
+			rom = "rom",
+		},
+	})
+
+	local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+	parser_config.uxn = {
+		install_info = {
+			url = "https://github.com/ASoldo/tree-sitter-uxn.git",
+			files = { "src/parser.c" },
+			branch = "main",
+		},
+		filetype = "uxn",
+	}
+
+	local highlights_scm = [[
+	(function) @function
+		]]
+
+	local runtime_path = vim.fn.stdpath("cache")
+	vim.opt.runtimepath:append(runtime_path)
+	vim.fn.mkdir(runtime_path .. "/queries/uxn", "p")
+
+	if vim.fn.isdirectory(runtime_path .. "/queries/uxn") == 1 then
+		local highlights_file = io.open(runtime_path .. "/queries/uxn/highlights.scm", "w")
+		io.output(highlights_file)
+		io.write(highlights_scm)
+		io.close(highlights_file)
+	end
+end
+
+return { setup = setup }
